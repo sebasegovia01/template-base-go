b7037a4a90a9c516158aec4de45f8a13498b0837
Sebastián Segovia S <ssegovias@bancoripley.com>
2024-03-11 11:05:36 -0300

    [Modify]: Reorganize code structure and remove unused code.

--- main.go
+++ main.go
@@ -48,89 +48,83 @@ func newHandlersContainer(servicesCont *services.Container) *handlers.Container 
 }
 
 func main() {
-
-	// init containers
 	utilsContainer := newUtilsContainer()
-
 	servicesContainer := newServicesContainer()
 
-	handlersContainers := newHandlersContainer(servicesContainer)
-
-	// Verifica si la aplicación se está ejecutando en AWS Lambda
 	_, isLambda := utilsContainer.Environment.LookupEnv("AWS_LAMBDA_RUNTIME_API")
 	fmt.Println("IsLambda?: ", isLambda)
+
 	if isLambda {
-		// Si es así, utiliza la función LambdaHandler como el manejador de la función Lambda
 		lambda.Start(LambdaHandler)
 	} else {
-		// environment arg
-		args := os.Args[1:]
-
-		var localEnv string
-		if len(args) > 0 {
-			localEnv = os.Args[1:][0] // just 1 arg is received
-		}
-
-		// parse from .env file if exists
-		if err := utils.ParseEnvironmentFile(localEnv); err != nil {
-			log.Fatal(err)
-		}
-
-		// CORS
-		headers := gorillaHandlers.AllowedHeaders([]string{"Content-Type", "Internal", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With"})
-		methods := gorillaHandlers.AllowedMethods([]string{"DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"})
-		origins := gorillaHandlers.AllowedOrigins([]string{"*"})
-
-		api := core.NewApi(handlersContainers, &utils.Logger{})
-
-		servicesContainer.Logger.Info("Initializing server")
-
-		port := fmt.Sprintf(":%v", utilsContainer.Environment.GetEnvVar("PORT"))
-
-		if port == ":" {
-			port = ":8080"
-		}
-
-		env := utilsContainer.Environment.GetEnvVar("ENV")
-
-		if env == "" {
-			env = "development"
-		}
-		servicesContainer.Logger.Info(fmt.Sprintf("Environment: %s", env))
-
-		serverURL := fmt.Sprintf("Server url: http://localhost%s", port)
-		servicesContainer.Logger.Info(serverURL)
-
-		swaggerURL := fmt.Sprintf("%s/api-docs/index.html", serverURL)
-		servicesContainer.Logger.Info(fmt.Sprintf("Swagger url: %s", swaggerURL))
-
-		log.Fatal(http.ListenAndServe(port, gorillaHandlers.CORS(headers, methods, origins)(api.Router())))
+		startHTTPServer(utilsContainer, servicesContainer)
 	}
 }
 
-// function to handler LambdaFunctionURLRequest
-// SE DEBE MODIFICAR EL EVENTO PARA HACER MATCH CON LA LLAMADA ESPECIFICA.
-func LambdaHandler(ctx context.Context, request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
-	// Inicializar contenedores como antes
+func startHTTPServer(utilsContainer *utils.Container, servicesContainer *services.Container) {
+	args := os.Args[1:]
+	localEnv := ""
+	if len(args) > 0 {
+		localEnv = args[0]
+	}
+
+	if err := utils.ParseEnvironmentFile(localEnv); err != nil {
+		log.Fatal(err)
+	}
+
+	api := setupAPI()
+	logServerInfo(utilsContainer, servicesContainer)
+
+	port := getServerPort(utilsContainer)
+	log.Fatal(http.ListenAndServe(port, setupCORS(api.Router())))
+}
+
+func setupAPI() core.Routes {
 	servicesContainer := newServicesContainer()
 	handlersContainers := newHandlersContainer(servicesContainer)
-	api := core.NewApi(handlersContainers, &utils.Logger{})
+	return core.NewApi(handlersContainers, &utils.Logger{})
+}
 
-	// Convertir el evento de LambdaFunctionURL a una solicitud HTTP utilizando httptest.NewRecorder para capturar la respuesta
+func setupCORS(handler http.Handler) http.Handler {
+	headers := gorillaHandlers.AllowedHeaders([]string{"Content-Type", "Internal", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With"})
+	methods := gorillaHandlers.AllowedMethods([]string{"DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"})
+	origins := gorillaHandlers.AllowedOrigins([]string{"*"})
+	return gorillaHandlers.CORS(headers, methods, origins)(handler)
+}
+
+func logServerInfo(utilsContainer *utils.Container, servicesContainer *services.Container) {
+	port := getServerPort(utilsContainer)
+	serverURL := fmt.Sprintf("Server url: http://localhost%s", port)
+	swaggerURL := fmt.Sprintf("%s/api-docs/index.html", serverURL)
+	servicesContainer.Logger.Info("Initializing server")
+	servicesContainer.Logger.Info(fmt.Sprintf("Environment: %s", utilsContainer.Environment.GetEnvVar("ENV")))
+	servicesContainer.Logger.Info(serverURL)
+	servicesContainer.Logger.Info(fmt.Sprintf("Swagger url: %s", swaggerURL))
+}
+
+func getServerPort(utilsContainer *utils.Container) string {
+	port := fmt.Sprintf(":%v", utilsContainer.Environment.GetEnvVar("PORT"))
+	if port == ":" {
+		port = ":8080"
+	}
+	return port
+}
+
+func LambdaHandler(ctx context.Context, request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
+	api := setupAPI()
+
 	req, err := utils.LambdaEventToHttpRequest(request)
 	if err != nil {
 		return events.LambdaFunctionURLResponse{StatusCode: http.StatusInternalServerError, Body: err.Error()}, nil
 	}
-	recorder := httptest.NewRecorder()
 
-	// Usar el enrutador para manejar la solicitud convertida
+	recorder := httptest.NewRecorder()
 	api.Router().ServeHTTP(recorder, req)
 
-	// Extraer la respuesta del recorder y convertirla a una respuesta de LambdaFunctionURL
 	response := events.LambdaFunctionURLResponse{
 		StatusCode: recorder.Code,
 		Body:       recorder.Body.String(),
-		Headers:    map[string]string{"Content-Type": "application/json"}, // Ajustar según sea necesario
+		Headers:    map[string]string{"Content-Type": "application/json"},
 	}
 
 	return response, nil
